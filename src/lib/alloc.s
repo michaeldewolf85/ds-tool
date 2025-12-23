301d2304e713d925fadd6741cb66903d44b23738
Mike DeWolf <mike.dewolf@veeder.com>
2025-12-23 11:08:24 -0500

    Perform code cleanup on alloc

--- src/lib/alloc.s
+++ src/lib/alloc.s
@@ -1,29 +1,36 @@
 # lib/alloc.s - Memory allocation utilities
 
+.include	"common.inc"
 .include	"linux.inc"
 
-.globl	alloc, free, realloc
+.globl	alloc, free
 
-# Private struct (used only in this file)
+# Private
+# Minimum bytes to request from SYS_BREAK
+.equ	NALLOC, 1<<8			# 256 bytes
+
+# Header for "alloc" memory block - 16 bytes
 	.struct	0
 Header.next:
-	.struct	Header.next + 1<<3	# Pointer to next block
+	.struct	Header.next + 1<<3	# Pointer to next free block
 Header.size:
-	.struct	Header.size + 1<<3	# Size of this block
+	.struct	Header.size + 1<<3	# Size of the block
 	.equ	HEADER_SIZE, .
 
-.equ	MIN_BRK_REQ, 1<<7			# Min break request (256 bytes) to save on syscalls
-
 .section .bss
+.align	16
 
+# Base of the block list. Declared with zeroes and initialized when "alloc" is first called
 base:
-	.zero	HEADER_SIZE	# Reserved space for the base of the list
+	.zero	HEADER_SIZE
 
+# Pointer to the "free list"
 freep:
-	.zero	HEADER_SIZE	# Reserved space to keep track of where we stopped searching last
+	.skip	1<<3
 
+# Current system break
 break:
-	.zero	1<<3		# Reserved space to track the current location of the break
+	.skip	1<<3
 
 .section .text
 
@@ -35,60 +42,123 @@ alloc:
 	push	%rbp
 	mov	%rsp, %rbp
 
-	# Determine the size needed for the requested space (in header size multiples). Note that 
-	# this can use a bitwise trick to round to the nearest multiple of the header size as it is
-	# a multiple of 2
-	add	$(2 * HEADER_SIZE - 1), %rdi	# Add space for a header and the aligment
-	and	$~(HEADER_SIZE - 1), %rdi	# Bitwise trick
+	# Store requested bytes somewhere less volatile
+	mov	%rdi, %rbx
 
-	# Checks to see if the base has already been initialized or if this is the first call
+	# Update the requested bytes to include size for a header and to be a multiple of header 
+	# size. This uses a bit twiddle to transform the value to a multiple of the header size
+	add	$(2 * HEADER_SIZE - 1), %rbx	# Add space for a header and header - 1 for bit op
+	and	$~(HEADER_SIZE - 1), %rbx	# Bitwise trick (NOT with header - 1)
+
+	# Checks to see if alloc has already been initialized
 	cmpq	$0, base
-	jne	.Lstart
+	je	init
 
-	mov	%rdi, %r12			# Save %rdi in %r12 for safekeeping. TODO 
-	call	alloc_init
+# Initialize vars for main loop
+# %rax will be current block
+# %rdx will be the previous block
+1:
+	mov	freep, %rdx			# Previous block initialized to the free pointer
+2:
+	mov	Header.next(%rdx), %rax		# Put current block being evaluated in %rax
 
-	mov	%r12, %rdi
-.Lstart:
-	# We will use %rcx to represent the previous block and %rax to represent the current block
-	movq	freep, %rcx			# previous pointer, address of previous block
+	# Check current block for enough space. There are three scenarios to track here: more than
+	# enough space, exactly enough space or not enough space
+	# space, exactly enough space or more than enough space
+	cmp	%rbx, Header.size(%rax)
+	je	unlink
+	jg	resize
 
-.Lscan:
-	movq	Header.next(%rcx), %rax		# Move the next (current) block into %rax
+	# If the current is equal to the free list pointer it means we have exhausted our search
+	# and need to request more memory
+	cmp	freep, %rax
+	je	more
 
-	# Check current block for space
-	cmp	%r12, Header.size(%rax)		# Check size against the memory requested
-	je	.Lexact
-	jg	.Lresize
+# Prepare for next loop iteration: set "previous" to "current"
+3:
+	mov	%rax, %rdx			# Set "previous" to "current"
+	jmp	2b
 
-	cmpq	freep, %rax			# Check if we are back at freep, if so we exhausted
-	jne	.Lnext				# all the options
-
-	call	morecore
-.Lnext:
-	mov	%rax, %rcx			# Move "curr" to "prev"
-	mov	Header.next(%rax), %rax		# Move "next" to "curr"
-	jmp	.Lscan
-
-.Lexact:
+# The current block has exactly the correct amount of space in which case we will unlink it from 
+# the "free list" and return it
+unlink:
 	# Exact size match, unlink this block from the free list
-	movq	Header.next(%rax), %rdx		# Move the "next" block's address into %rdx
-	movq	%rdx, Header.next(%rcx)		# Set the "prev" block to point to the "next"
-	jmp	.Ldone				# effectively removing the current from rotation
+	mov	Header.next(%rax), %rsi		# Move the "next" block's address into %rsi
+	mov	%rsi, Header.next(%rdx)		# Set the "prev" block to point to the "next"
+	jmp	6f				# effectively removing the current from rotation
 
-.Lresize:
+# The current block as more than enough space in which case we will shrink it and a chunk at the 
+# tail end to the caller
+resize:
 	# Block is greater than needed so we resize it and return the tail end
-	sub	%r12, Header.size(%rax)		# Subtract requested from size of current block
-	add	Header.size(%rax), %rax		# Move %rax pointer to the start of the returned
-	mov	%r12, Header.size(%rax)		# Set the size on the new block
+	mov	Header.size(%rax), %rsi		# Move header size into %rsi for subtraction
+	sub	%rbx, %rsi			# Calculate the new size
+	mov	%rsi, Header.size(%rax)		# Store the new size for the free block
+	add	%rsi, %rax			# Move %rax pointer to the memory we will return
+	mov	%rbx, Header.size(%rax)		# Set the size on the returned block
 
-.Ldone:
-	movq	%rcx, freep			# Store where we left off searching in freep
-	add	$HEADER_SIZE, %rax		# %rax has the return value but we want to skip
-						# the header
+6:
+	mov	%rdx, freep			# Store where we left off searching in freep
+	add	$HEADER_SIZE, %rax		# Increment the returned pointer past the header
+						# i.e. point it to the "useable" memory
+7:
 	mov	%rbp, %rsp
 	pop	%rbp
 	ret
+
+# Initialize alloc (if this is the first time it is being called)
+init:
+	movq	$base, base + Header.next	# Point the base pointer at itself
+	movq	$base, freep			# Point the free pointer at the base
+
+	# Obtains the current system break by calling SYS_BRK with 0
+	mov	$SYS_BRK, %rax
+	mov	$0, %rdi
+	syscall	
+
+	mov	%rax, break			# Store the location of the break
+	jmp	1b
+
+# Request more memory from the OS
+more:
+	# Move requested bytes into %rdx to manipulate it
+	mov	%rbx, %rdi
+
+	# Check the request against the minimum number of bytes and increase if necessary
+	mov	$NALLOC, %rdx
+	cmp	%rdx, %rdi
+	cmovb	%rdx, %rdi
+
+	# Preserve the requested bytes as we will need it later ...
+	mov	%rdi, %rdx
+
+	# Request the new break, add the requested size to the old break in rdi
+	mov	$SYS_BRK, %rax
+	add	break, %rdi
+	syscall
+
+	# SYS_BREAK returns negative one in the error case so we need to check for that and cast
+	# %rax to NULL if this is so
+	mov	$NULL, %rsi		# Necessary because we cannot cmov an immediate
+	cmp	$-1, %rax
+	cmove	%rsi, %rax
+	je	7b
+
+	# The old system break is the pointer to our new memory block. We move that into %rdi in
+	# preparation for a call to free
+	mov	break, %rdi
+
+	# Update our break to the new value. We need to do this AFTER storing the old in %rdi
+	mov	%rax, break
+
+	# Set the "size" our new block in its header and increment the pointer to the useable
+	# memory which is what "free" expects to receive. Call free which will insert the
+	# block into the "free list". Finally return back to the scan loop so that the new block
+	# can be discovered
+	mov	%rdx, Header.size(%rdi)
+	add	$HEADER_SIZE, %rdi
+	call	free
+	jmp	3b
 
 # Free previously allocated memory and coallesce it into the "free list"
 # @param	%rdi	Memory address of the block to free
@@ -98,129 +168,101 @@ free:
 	push	%rbp
 	mov	%rsp, %rbp
 
-	sub	$HEADER_SIZE, %rdi		# Point rdi at start of block
+	sub	$HEADER_SIZE, %rdi	# Point %rdi at start of our block's header
 
-	mov	freep, %rax			# Sets "current" search block to free pointer
+	mov	freep, %rax		# Initialize %rax to the start of the "free list"
 
-	# Find adjoining block
-.Lfind:
-	cmp	%rax, %rdi			# Compare our blocks address to "curr"
-	jle	.Lcontinue			# If our block is less than the "curr" go to "next"
+# Finds the preceding block in the "free list"
+# %rax is the current block 
+# %rdx is the next
+1:
+	mov	Header.next(%rax), %rdx	# Move "next" into %rdx
 
-	cmp	Header.next(%rax), %rdi		# Compare our blocks address to "next" of "curr"
-	jge	.Lfound
+	# Checks if our block is less than or equal to the current. If it is we can skip the next
+	# check
+	cmp	%rax, %rdi
+	jle	2f
 
-.Lcontinue:
-	mov	Header.next(%rax), %rax		# Increment "curr" to next item
-	jmp	.Lfind
+	# If we are here, our block is greater than the current. If it is less than the next we
+	# found our spot
+	cmp	%rdx, %rdi
+	jl	check_next
 
-.Lfound:
-	# We now have the adjoining "found" block in %rax
+# We have not found our spot yet. We need to check if the "current" is the end of the list and if
+# so, our block may preceed or succeed the list
+2:
+	# Checks if "current" is greater than the "next" it points to which means end of list
+	cmp	%rdx, %rax
+	jge	end_of_list
 
-	mov	%rdi, %rdx			# Point %rdx to the "end" of our block
-	add	Header.size(%rdi), %rdx
+# Tee up the next iteration of the loop
+3:
+	mov	Header.next(%rax), %rax
+	jmp	1b
 
-	movq	Header.next(%rax), %r8		# Store next in r8 so we can use it
-	cmp	%rdx, %r8			# If the end of the our block abuts the "next" of
-	je	.Ljoin_next			# "found" the two blocks will be joined
+# We've found our block in %rax. Next we need to insert it into the free list
+check_next:
+	# Put the end of our block in %rsi
+	mov	%rdi, %rsi
+	add	Header.size(%rdi), %rsi
 
-.Lset_next:
-	# Otherwise we know that our block points the the "next" of "found"
-	movq	%r8, Header.next(%rdi)
+	# Check if the end of out block adjoins with the "next" and join it with "next" if so
+	cmp	%rdx, %rsi
+	je	adjoin_next
 
-.Lcheck_prev:
-	cmp	%rdi, %rax			# If the start of the new block abuts "found" the
-	je	.Ljoin_prev			# two blocks will be joined
+	# If out block does not adjoin with "next" it should point at the "next" of current
+	mov	%rdx, Header.next(%rdi)
 
-	# Otherwise "found" will have its "next" pointing to our block
+# Check if our block adjoins with "current" and join it with "current" if so
+check_prev:
+	cmp	%rdi, %rax
+	je	adjoin_prev
+
+	# If our block does not adjoin "current" then current should point to our block
 	mov	%rdi, Header.next(%rax)
-	jmp	.Lend
 
-.Ljoin_next:
-	mov	Header.size(%rax), %rsi			# Puts size of "next" into %rsi
-	add	%rsi, Header.size(%rdi)			# Adds size of "next" to our block
-	movq	%r8, Header.next(%rdi)			# Adjust next pointer of our block to after
-	jmp	.Lcheck_prev
-
-.Ljoin_prev:
-	mov	Header.size(%rdi), %rsi			# Puts size of our block in %rsi
-	add	%rsi, Header.size(%rax)			# Adds size of out block to "prev"
-	movq	%r8, Header.next(%rax)			# Adjust next pointer of "found" to point
-							# After our block
-.Lend:
-	mov	%rax, freep				# Save "found" as the free pointer
+# Done adding freed block to the "free list"
+3:
+	# Move the free pointer to the current block since we know there is now a free block after
+	# it
+	mov	%rax, freep
 
 	mov	%rbp, %rsp
 	pop	%rbp
 	ret
 
-# Resize an allocation previously allocated with "alloc". Will make every attempt to perform the
-# reallocation "in-place" without relocating the block. If the memory needs to be relocated the 
-# existing contents of the block are moved to the new location
-# @param	%rdi	Address of the memory block to reallocate
-# @param	$rsi	The new size to allocate
-# @param	%rax	Address of the resized memory block
-.type	realloc, @function
-realloc:
-	push	%rbp
-	mov	%rsp, %rbp
+# We are at the end of the list (ie it wraps around). We need to check if out block is either 
+# BEFORE or AFTER the list
+end_of_list:
+	# Our block is BEFORE the "next" (which is the start of the list)
+	cmp	%rdx, %rdi
+	jl	check_next
 
-	mov	%rbp, %rsp
-	pop	%rbp
-	ret
+	# Our block is AFTER the current (which is the end of the list)
+	cmp	%rax, %rdi
+	jg	check_next
 
-# Private function to initialize alloc
-# @return	void
-.type	alloc_init, @function
-alloc_init:
-	push	%rbp
-	mov	%rsp, %rbp
+	# Our block is somewhere in the middle of the list
+	jmp	3b
 
-	# Intialization logic ... only runs if this is the first time alloc is called
-	movq	$base, base			# Set base pointer to point to itself
-	movq	$base, freep			# Initialize the free pointer to the base
+# Our block adjoins with "next" (which is in %rsi) so we need to join the two
+adjoin_next:
+	# Puts the size of the "next" block in %rdx and adds that value to the size of our block
+	mov	Header.size(%rsi), %rdx
+	add	%rdx, Header.size(%rdi)
 
-	# Obtain the current break ...
-	mov	$SYS_BRK, %rax			# Make the break syscall...
-	mov	$0, %rdi			# With parameter 0
-	syscall					# Which returns the current location of the break
+	# Move the "next" pointer of the "next" block into our block to complete the merge
+	mov	Header.next(%rsi), %rdx
+	mov	%rdx, Header.next(%rdi)
+	jmp	check_prev
 
-	mov	%rax, break			# Store the location of the break
+# Our block adjoins with "current" so we need to join the two
+adjoin_prev:
+	# Moves the size of our block into %rdx and adds it to the size of the "current"
+	mov	Header.size(%rdi), %rdx
+	add	%rdx, Header.size(%rax)
 
-	mov	%rbp, %rsp
-	pop	%rbp
-	ret
-
-# Privaet function to request more memory from OS and insert into the "free list"
-# @param	%rdi	Amount of memory to request
-# @return	???
-.type	morecore, @function
-morecore:
-	push	%rbp
-	mov	%rsp, %rbp
-
-	push	%r12				# Preserve r12
-
-	cmp	$MIN_BRK_REQ, %rdi
-	jge	.Lrequest
-
-	mov	$MIN_BRK_REQ, %rdi		# Ensures we request at LEAST the min bytes
-	mov	%rdi, %r12			# Save the size we requested in %r12
-
-.Lrequest:
-	mov	$SYS_BRK, %rax			# Make the break syscall...
-	add	break, %rdi			# calculate the new break
-	syscall					# Which returns the current location of the break
-
-	mov	break, %rdi			# Move the "old" break into %rdi because it points
-	mov	%r12, Header.size(%rdi)		# Set the header size of the block
-	mov	%rax, break			# Keep track of the "new" break
-
-	add	$HEADER_SIZE, %rdi		# and point past the header as that is what "free"
-	call	free				# expects. Free will add new memory to "free list"
-
-	pop	%r12				# Restore r12
-
-	mov	%rbp, %rsp
-	pop	%rbp
-	ret
+	# Moves the "next" pointer of our block to be the "next" pointer of "the current"
+	mov	Header.next(%rdi), %rdx
+	mov	%rdx, Header.next(%rax)
+	jmp	3b
