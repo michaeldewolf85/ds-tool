aa2b78a5b337d0d7827a3a8d5c7bae149fe4137b
Mike DeWolf <michaeldewolf85@gmail.com>
2026-01-11 16:25:20 -0500

    Adds "help" command (#43)
    Co-authored-by: Mike DeWolf <mike.dewolf@veeder.com>

--- src/proc/evaluate.s
+++ src/proc/evaluate.s
@@ -1,15 +1,18 @@
 # proc/evaluate.s - Evaluate parsed input and delegate to the appropriate handler
 
+.include	"common.inc"
 .include	"linux.inc"
 .include	"structs.inc"
 
-.globl	evaluate
+.globl	evaluate, evaluate_commands
 
 .section .rodata
 
-commands:
+.type	evaluate_commands, @object
+evaluate_commands:
 	.quad	exit
 	.quad	ping
+	.quad	help
 	.quad	arraystack
 	.quad	arrayqueue
 	.quad	arraydeque
@@ -23,6 +26,7 @@ commands:
 handlers:
 	.quad	exit_handler
 	.quad	ping_handler
+	.quad	help_handler
 	.quad	arraystack_handler
 	.quad	arrayqueue_handler
 	.quad	arraydeque_handler
@@ -42,7 +46,7 @@ handlers:
 evaluate:
 	push	%rbp
 	push	%rbx				# Pointer to input struct
-	push	%r12				# Index into commands array
+	push	%r12				# Index into evaluate_commands array
 	mov	%rsp, %rbp
 
 	mov	%rdi, %rbx
@@ -51,7 +55,7 @@ evaluate:
 	xor	%r12, %r12			# Zero out an index register
 
 check:
-	mov	commands(, %r12, 8), %rsi
+	mov	evaluate_commands(, %r12, 8), %rsi
 	cmp	$0, %rsi			# Check for the sentinel, if we match here the 
 	je	match				# command was not found
 
