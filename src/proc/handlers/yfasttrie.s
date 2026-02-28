c1f59ce1dd721e73247b591906c04a41028f7e4e
Mike DeWolf <michaeldewolf85@gmail.com>
2026-02-28 15:01:21 -0500

    Implements YFastTrie accessory structures (#78) (#79)
    Co-authored-by: Mike DeWolf <mike.dewolf@veeder.com>

---
+++ src/proc/handlers/yfasttrie.s
@@ -0,0 +1,43 @@
+# proc/handlers/yfasttrie.s - Handler for the "yfasttrie" command
+
+.include	"common.inc"
+.include	"structs.inc"
+
+.globl	yfasttrie, yfasttrie_handler
+
+.section .bss
+
+this:
+	.zero	1<<3
+
+.section .rodata
+
+yfasttrie:
+	.ascii	"yfasttrie\0"
+
+.section .text
+
+# @function	yfasttrie_handler
+# @description	Handler for the "yfasttrie" command
+# @param	%rdi	User input
+# @return	void
+.type	yfasttrie_handler, @function
+yfasttrie_handler:
+	push	%rbp
+	mov	%rsp, %rbp
+
+	cmpq	$NULL, this
+	jne	1f
+
+	call	YFastTrie_ctor
+	mov	%rax, this
+
+1:
+	mvo	this, %rdi
+
+	mov	$1, %rsi
+	call	YFastTrie_add
+
+	mov	%rbp, %rsp
+	pop	%rbp
+	ret
